32630bd312ecc5b1e093571ae954e2a63f037354
Uzair Ashraf <uzinatorcl@live.com>
2019-09-10 19:55:29 -0700

    updated api link

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 $(document).ready(initializeApp)
+var pokemonData = {}
 
 function initializeApp() {
 
 }
 
 function fetchPokemonFromServer() {
+  pokemonData = {
+    'url': //https://pokeapi.co/api/v2/pokemon/?limit=151
+  }
+}
 
+function pokemonRecievedSuccessfully(data) {
+  console.log(data);
 }
