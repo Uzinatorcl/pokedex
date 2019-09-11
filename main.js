85e233eae4b73a6a0bd04a31ed68325c97425c44
Uzair Ashraf <uzinatorcl@live.com>
2019-09-11 14:41:54 -0700

    pokedex listed properly

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 $(document).ready(initializeApp)
-var pokemonData = {}
+
+var pokemonDataSent = {};
+var pokemonDataRecieved = {};
 
 function initializeApp() {
+fetchPokemonFromServer();
+addEventListeners();
+}
 
+function addEventListeners() {
+  $('body').on('click', '.pokemon', displayPokemon);
 }
 
 function fetchPokemonFromServer() {
-  pokemonData = {
-    'url': //https://pokeapi.co/api/v2/pokemon/?limit=151
+  pokemonDataSent = {
+    'url': "https://pokeapi.co/api/v2/pokemon/?limit=151",
+    success: pokemonRecievedSuccessfully
   }
+  $.ajax(pokemonDataSent)
 }
 
 function pokemonRecievedSuccessfully(data) {
-  console.log(data);
+  pokemonDataRecieved = data;
+
+  for(var pokeIndex = 0; pokeIndex < pokemonDataRecieved.results.length; pokeIndex++) {
+    var pokeID = "" + (pokeIndex + 1);
+    var pokemonContainer = $('<div>').addClass('pokemon ' + pokemonDataRecieved.results[pokeIndex].name);
+    var pokemonID = $('<div>').addClass('pokemonID').text(pokeID);
+   //debugger;
+    var pokemonName = $('<div>').addClass('pokemonName').text(pokemonDataRecieved.results[pokeIndex].name);
+    pokemonContainer.append(pokemonID, pokemonName);
+    $('.interact').append(pokemonContainer);
+  }
+
+}
+
+function displayPokemon(event) {
+  var pokemonToRequestIndex = parseInt(event.currentTarget.firstChild.textContent) - 1
+  console.log(pokemonDataRecieved.results[pokemonToRequestIndex].url);
 }
