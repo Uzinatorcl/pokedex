587e5d3a63147b0fef9043f22b0c47aeb763cbda
Uzair Ashraf <uzinatorcl@live.com>
2019-09-10 18:19:05 -0700

    basic oop and main js files setup

---
+++ pokemon.js
@@ -0,0 +1,13 @@
+class Pokemon {
+  constructor(name) {
+    this.pokemonName = name;
+    this.domElements = {
+      displayText : $('.displayText'),
+      displayImage: $('.displayImage')
+    }
+  }
+  render() {
+    //render the pokemon
+  }
+
+}
