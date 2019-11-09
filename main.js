5459a8fce4754651c6b10d35dfcc05b5dae92704
Shadoki <uzinatorcl@gmail.com>
2019-11-09 15:10:22 -0800

    scroll combined with button scrolls

--- main.js
+++ main.js
@@ -52,6 +52,10 @@ function pokemonDataRecievedSuccessfully(data) {
 
 function displayPokemon(event) {
   loadingScreen();
+  $('.currentSelection').removeClass('currentSelection');
+  $(event.currentTarget).addClass('currentSelection');
+  scrollPosition = $('.interact').scrollTop() + $(event.currentTarget).position().top
+  $('.interact').scrollTop(scrollPosition);
   var pokemonToRequestIndex = parseInt(event.currentTarget.firstChild.textContent) - 1
   var pokemonToRequestURL = pokemonDataRecieved.results[pokemonToRequestIndex].url
   var pokemonToRequest = {
